2a72e0c59ef9f72cc001c2dbc0b6ec5b8de83fcd
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-08-24 23:09:05 -0400

    creo la pagina amenazas y se agregaron alt

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -2,3 +2,9 @@ validarEmail = (email) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(String(email).toLowerCase());
 }
+
+function pausarVideo(id) {
+    var div = document.getElementById(id);
+    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+    iframe.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}','*');
+}
